c6aef36a87633a8905f131c2cb9c30205f681a90
Phylliida Dev <phylliida.dev@gmail.com>
2022-11-15 11:21:38 -0500

    make half and fix saving

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -730,7 +730,7 @@ def main():
                 pipeline.text_encoder.save_pretrained(frz_dir)
                          
             if args.save_n_steps >= 1:
-               if global_step < args.max_train_steps-100 and global_step+1==i:
+               if global_step % args.save_n_steps == args.save_n_steps-1:
                   ckpt_name = "_step_" + str(global_step+1)
                   save_dir = Path(args.output_dir+ckpt_name)
                   save_dir=str(save_dir)
@@ -748,7 +748,11 @@ def main():
                      #      text_encoder=accelerator.unwrap_model(text_encoder),
                      #)
                      #pipeline.save_pretrained(save_dir)
-                     torch.save(accelerator.unwrap_model(unet).half().state_dict(), str(Path(args.Session_dir+"/" + inst + "unet.pkl")))
+                     unet_unwrap = accelerator.unwrap_model(unet)
+                     dt = unet_unwrap.dtype
+                     unet_unwrap.half()
+                     torch.save(unet_unwrap.state_dict(), str(Path(args.Session_dir+"/" + inst + "unet.pkl")))
+                     unet_unwrap.type(dt)
                      #frz_dir=args.output_dir + "/text_encoder_frozen"                    
                      #if args.train_text_encoder and os.path.exists(frz_dir):
                      #   subprocess.call('rm -r '+save_dir+'/text_encoder/*.*', shell=True)
