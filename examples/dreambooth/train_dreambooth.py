2fb684dcfe739a046738c85717b17da25277847a
Phylliida Dev <phylliida.dev@gmail.com>
2022-12-13 16:27:06 -0500

    Update train_dreambooth.py

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -385,7 +385,7 @@ class DreamBoothDataset(Dataset):
             instance_prompt,
             padding="do_not_pad",
             truncation=True,
-            max_length=self.tokenizer.model_max_length,
+            max_length=tokenizer.model_max_length,
         ).input_ids
 
         if self.class_data_root:
