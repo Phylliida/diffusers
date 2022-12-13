7541d86460bfbb46b3674206a226847ca598ae9a
Phylliida Dev <phylliida.dev@gmail.com>
2022-12-13 16:37:30 -0500

    fix batch di

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -618,8 +618,7 @@ def main(discordQueue):
         truncation=True,
         max_length=tokenizer.model_max_length,
     ).input_ids
-    starting = tokenizer.pad({"input_ids": input_ids}, padding=True, return_tensors="pt").input_ids
-    print(starting)
+    starting = tokenizer.pad({"input_ids": input_ids}, padding=True, return_tensors="pt").input_ids.view(1, -1)
         
     encoder_hidden_states = text_encoder(starting)[0]
     
